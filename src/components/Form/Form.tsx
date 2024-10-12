dbc78705739762d3c32c4dca72d0d7b8bdf20dc6
AnyaLap <anya-serysheva@yandex.ru>
2024-10-12 14:09:24 +0300

    props/useState

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react'
 import classes from './Form.module.scss'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const Form = () => {
+export const Form = (props: {createNewToDo: Function}) => {
+
+    const [text,setText] = useState<string>('')
+
+    const formSubmit = () => {
+        if (text) {
+            props.createNewToDo(text)
+            toast("Задача добавлена!")
+            setText('')
+        }
+    }
+
     return (
+        
         <div className={classes.wrapper}>
-            <form action="#">
+            <form action="#" onSubmit = {formSubmit}>
                 <label>
-                    <input className={classes.formInput} type="text" />
-                    <button className={classes.formBtn} ></button>
+                    <input 
+                      value={text}
+                      className={classes.formInput}
+                      onChange={(e)=>setText(e.target.value)}
+                      type="text" />
+                    <div>
+                        <button 
+                        className={classes.formBtn}>
+                        </button>
+                    </div>
                 </label>
             </form>
+            <ToastContainer position='bottom-right'/>
         </div>
     )
 }
